934fda4e1689b98abfc93d6dea27d53fe092eb4d
Golos1 <68024905+Golos1@users.noreply.github.com>
2025-06-23 20:22:28 -0400

    Initial commit with AWS Lambda Actor

---
+++ lambda/lambdaActor.go
@@ -0,0 +1,86 @@
+package faas_akt
+
+import (
+	"encoding/json"
+
+	"github.com/aws/aws-sdk-go-v2/service/lambda"
+	"github.com/aws/aws-sdk-go-v2/service/lambda/types"
+	"github.com/go-playground/validator/v10"
+	"github.com/tochemey/goakt/v3/actor"
+	"github.com/tochemey/goakt/v3/goaktpb"
+)
+
+// Actor which invokes a lambda function, taking parameters to the function as a JSON string and
+// optionally returning the last 4 KB of the function logs as well. The consumer is responsible
+// for initializing the lambdaClient and ensuring it has the permissions neccesary to invoke functions.
+// The parameters are validated against the supplied type parameter.
+type LambdaActor[T any] struct {
+	functionName string
+	getLogs      bool
+	client       *lambda.Client
+}
+
+// Initializes a new Actor. getLogs being true only fetches the last 4 KB, not the whole logs of a function call.
+func NewLambdaActor[T any](functionName string, client *lambda.Client, getLogs bool) LambdaActor[T] {
+	return LambdaActor[T]{
+		functionName: functionName,
+		client:       client,
+		getLogs:      getLogs,
+	}
+}
+
+// This Actor doesn't have any setup for PreStart to do.
+func (actor LambdaActor[T]) PreStart(ctx *actor.Context) error {
+	return nil
+}
+
+// This Actor doesn't have any teardown for PostStop to do.
+func (actor LambdaActor[T]) PostStop(ctx *actor.Context) error {
+	return nil
+}
+
+// If the supplied parameters fail validation, the
+// The Result of the invocation will be turned into a json string and sent back.
+func (actor LambdaActor[T]) Receive(ctx *actor.ReceiveContext) {
+	var args T
+	switch ctx.Message().(type) {
+	case *goaktpb.PostStart:
+	case *Params:
+		parameters := ctx.Message().ProtoReflect().Get(ctx.Message().ProtoReflect().Descriptor().Fields().ByName("JsonParamString")).String()
+		arg_bytes := []byte(parameters)
+		err := json.Unmarshal(arg_bytes, &args)
+		if err != nil {
+			ctx.Logger().Error("Error parsing parameters: ", err)
+			ctx.Unhandled()
+		}
+		validator := validator.New()
+		err = validator.Struct(args)
+		if err != nil {
+			ctx.Logger().Error("Error validating schema: ", err)
+			ctx.Unhandled()
+		}
+		logType := types.LogTypeNone
+		if actor.getLogs {
+			logType = types.LogTypeTail
+		}
+		result, err := actor.client.Invoke(ctx.Context(), &lambda.InvokeInput{
+			FunctionName: &actor.functionName,
+			LogType:      logType,
+			Payload:      arg_bytes,
+		})
+		if err != nil {
+			ctx.Logger().Error("Error Invoking Function: ", err)
+			ctx.Unhandled()
+
+		} else {
+			ctx.Logger().Info("Function successfully invoked.")
+			reply := new(Result)
+			payload, _ := json.Marshal(result.Payload)
+			reply.JsonResultString = string(payload)
+			reply.Logs = *result.LogResult
+			ctx.Response(reply)
+		}
+	default:
+		ctx.Unhandled()
+	}
+}
---
+++ lambda/lambdaActor_test.go
@@ -0,0 +1,61 @@
+package faas_akt
+
+import (
+	"context"
+	"encoding/json"
+	"testing"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/service/lambda"
+	"github.com/tochemey/goakt/v3/actor"
+	"github.com/tochemey/goakt/v3/log"
+)
+
+type AddParams struct {
+	a int `validate:"required"`
+	b int `validate:"required"`
+}
+type AddResult struct {
+	result int `validate:"required"`
+}
+
+func TestLambdaActor(t *testing.T) {
+
+	ctx := context.Background()
+	logger := log.DefaultLogger
+	actorSystem, _ := actor.NewActorSystem(
+		"TestLambda",
+	)
+	if err := actorSystem.Start(ctx); err != nil {
+		logger.Error(err)
+		t.Error("Failed to start Actor System", err)
+	}
+	client := lambda.Client{}
+	pid, err := actorSystem.Spawn(ctx, "Lambda", NewLambdaActor[AddParams]("Add", &client, true))
+	if err != nil {
+		logger.Error(err)
+		t.Error("Failed to spawn actor", err)
+	}
+	params := new(Params)
+	numberBytes, err := json.Marshal(AddParams{a: 2, b: 3})
+	if err != nil {
+		logger.Error(err)
+		t.Error("Failed to spawn actor", err)
+	}
+	params.JsonParamString = string(numberBytes)
+	response, err := actor.Ask(ctx, pid, params, time.Second)
+	if err != nil {
+		logger.Error(err)
+		t.Error("Failed to message actor", err)
+	}
+	switch response.(type) {
+	case *Result:
+		descriptor := response.ProtoReflect().Descriptor().Fields().ByName("JsonParamString")
+		jsonResult := response.ProtoReflect().Get(descriptor)
+		var structResult AddResult
+		json.Unmarshal(jsonResult.Bytes(), &structResult)
+		if structResult.result != 5 {
+			t.Error("Add should have returned 5", structResult.result, 5)
+		}
+	}
+}
